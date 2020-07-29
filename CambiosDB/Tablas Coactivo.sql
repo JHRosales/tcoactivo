8b5bf3a8e845529608bfde130db9a1927b033315
Jhimi Rosales <jhimi.2201@gmail.com>
2020-07-29 18:03:18 -0500

    Cambios tablas

---
+++ CambiosDB/Tablas Coactivo.sql
@@ -0,0 +1,24 @@
+﻿select * from coactivo.masunto  	-- Tabla Maestra (Data estable) Asuntos para el expediente Coactivo
+select * from coactivo.dasunto 		-- Tabla Maestra (Data estable) Detalle de asuntos
+select * from coactivo.mconten  	-- Tabla Maestra (Data estable) Datos generales Depurar
+
+select * from coactivo.auxiliarcoactivo -- Auxiliares Coactivos y Ejecutor Coactivo
+select * from coactivo.correldocum	-- Correlativo de los documentos o Expedientes por años
+select * from coactivo.costasprocesales -- Costas Procesales datos generales
+select * from coactivo.dasunto_req 	-- Destalle de Asuntos requeridos ?
+select * from coactivo.dcostasprocesales -- Detalle de las costas procesales (Tarifas)
+select * from coactivo.ddocumento  	-- Detalle de los expedientes creados
+select * from coactivo.ddocumestr  	-- ??
+select * from coactivo.doc_emitidodet 	-- Documentos emitidos detalle ?
+select * from coactivo.doc_emitidos 	-- Documentos emitidos ?
+select * from coactivo.druta 		-- Detalle de la ruta??
+select * from coactivo.expedientescostas  -- Costas procesales agregados a cada expediente
+select * from coactivo.madjunto  	-- Archivos Adjuntos al Expediente Coactivo
+select * from coactivo.mconfigdoc  	-- ??
+select * from coactivo.mcorusudoc  	-- ??
+select * from coactivo.mdocumento 	-- datos generales del Expediente coactico
+select * from coactivo.mruta  		-- detalle de los movimientos transferencias del expediente
+select * from coactivo.mruta_audit  	-- registro de auditoria de la tala mruta (cuando se elimina una movimiento)
+select * from coactivo.pagostesoreria   -- pagos para la pagina principal del Modulo Coactivo
+
+
